47e797165a3d6caa42d1d8ed272de29c8f1297c9
Pavel Gorbachev <truepaulberg5@gmail.com>
2022-04-24 12:16:30 +0300

    My first JS commit

---
+++ git/script.js
@@ -0,0 +1,46 @@
+// "Создать форму - 2 инпута даты и кнопка ""Рассчитать"". При нажатии на кнопку получить курс доллара с сайта нацбанка на все даты в диапазоне и вывести на интерфейс дату с минимальным и максимальным курсами(с указанием курса). Кнопку можно нажать только есть обе даты введены и диапазон корректен (то есть левая дата меньше правой). Одним запросом можно получать курс только на один день(нельзя использовать эндпоинт на получение курсов на диапазон дат)
+// ДЗ разместить на GIT скинуть ссылку на репозиторий"
+function createDate(dateValueMs) {
+    let currentDate
+    if (dateValueMs) {
+        currentDate = new Date(dateValueMs)
+    } else {
+        currentDate = new Date()
+    }
+    let currentDay = currentDate.getDate()
+    if (currentDay < 10) {
+        currentDay = `0${currentDay}`
+    }
+    let currentMonth = currentDate.getMonth() + 1
+    if (currentMonth < 10) {
+        currentMonth = `0${currentMonth}`
+    }
+    let currentYear = currentDate.getFullYear()
+    return `${currentYear}-${currentMonth}-${currentDay}`
+}
+let maxDate = createDate()
+console.log(maxDate)
+document.querySelector('#count').addEventListener('click', () => {
+    let dateFrom = document.querySelector('#dateFrom').value
+    let dateTo = document.querySelector('#dateTo').value
+    if ((dateFrom || dateTo) > maxDate || dateFrom > dateTo || (dateFrom || dateTo) === '') {
+        return
+    }
+    function getDates(dateFrom, dateTo) {
+        let arrayOfDates = []
+        let startDateMs = Date.parse(dateFrom)
+        while (startDateMs <= Date.parse(dateTo)) {
+            startDateMs += 24 * 3600 * 1000
+            let date = createDate(startDateMs)
+            arrayOfDates.push(date)
+        }
+        return arrayOfDates
+    }
+    getDates(dateFrom, dateTo)
+    console.log(getDates(dateFrom, dateTo))
+    let getFetchList = getDates(dateFrom, dateTo).map(date => fetch(`https://www.nbrb.by/api/exrates/rates/usd?parammode=2&ondate=${date}`).then(response => response.json()).then(response => response.Cur_OfficialRate))
+    Promise.all(getFetchList).then(values => {
+        document.querySelector('#out').innerHTML = `min USD rate: ${Math.min(...values)}</br> max USD rate: ${Math.max(...values)}`
+    })
+})
+
